1def562a5c1dd149a781ca3d1849a3903802cf23
Mati Kalev Kiisler <matikalev@hotmail.com>
2023-08-29 00:39:30 +0300

    Movie page using API hook, accessing basic info

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,43 +1,26 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import axios from "axios";
-import { useState } from "react";
-
-interface MovieDetails {
-  title: string;
-  description: string;
-  image: string;
-  genres: string[];
-}
+import Image from "next/image";
+import useMovie from "./../../../hooks/useMovie";
 
 export default function Movie({ params }: { params: { id: string } }) {
-  const [movieDetails, setMovieDetails] = useState<MovieDetails>();
-  const movieDetailsURL =
-    "https://yts.mx/api/v2/movie_details.json?movie_id=" +
-    params.id +
-    "&with_images=true&with_cast=true";
-  axios.get(movieDetailsURL).then(
-    (response) => {
-      const data = response.data;
-      const details: MovieDetails = {
-        title: data.title_long,
-        description: data.description_full,
-        image: data.background_image,
-        genres: data.genres,
-      };
-      setMovieDetails(details);
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
-  console.log(movieDetails);
+  const { data: movie, isLoading, isError, error } = useMovie(params.id);
+  console.log(movie);
+  if (movie === undefined) {
+    return "loading...";
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        {movieDetails?.title}
+        {movie.title} ({movie.year})
       </div>
+      <Image
+        src={movie.mediumCoverImage}
+        alt={`Picture of ${movie.title}`}
+        height={200}
+        width={200}
+      />
+      <div>{movie.genres.join(", ")}</div>
     </main>
   );
 }
