61c5673394d2626a250485fbb79ce1dce7854791
Rasmus Aasmäe <s2031389@ed.ac.uk>
2023-09-09 14:54:12 +0300

    limit results to 20

--- src/libs/supabase/get-movies.ts
+++ src/libs/supabase/get-movies.ts
@@ -2,11 +2,14 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import type { Database } from "./types";
 
 export default async function getMovies(query: string) {
+  if (query === "") return [];
   const supabase = createClientComponentClient<Database>();
   const movies = await supabase
     .from("movies")
     .select()
     .ilike("title", `%${query}%`)
-    .order("imdb_rating", { ascending: false, nullsFirst: false });
+    .order("imdb_rating", { ascending: false, nullsFirst: false })
+    .order("year", { ascending: false, nullsFirst: false })
+    .limit(20);
   return movies.data ?? [];
 }
