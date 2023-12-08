ec5f375acffd8447e26ab432b9611c42af00c5fd
YEEEEEEEEEEP <116300422+YEEEEEEEEEEP@users.noreply.github.com>
2023-12-08 22:25:00 +0200

    We landing on SHREK

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
+import MovieCard from "@/components/movie-card";
+
 export default function Home() {
+  const myArray = ["a", "asd", "Dsa", "as", "dsda", "ddkkk", "ad", "ss"];
+
   return (
-    <main className="w-screen min-h-screen pb-8 flex flex-col items-center gap-6"></main>
+    <main
+      className="w-screen border border-red-300 pb-8 flex flex-col items-center gap-6 mt-10">
+      <div className="relative h-64 w-full max-w-7xl">
+        <div
+          className="flex flex-row space-x-4 h-full w-full overflow-x-auto border-yellow-200">
+          {
+            myArray.map(val => (
+                <MovieCard tmdb_id={808} title={val}
+                           poster_path={"2yYP0PQjG8zVqturh1BAqu2Tixl.jpg"}
+                           key={val} />
+              ),
+            )
+          }
+
+        </div>
+        <div
+          className="w-24 h-full bg-gradient-to-r from-transparent to-slate-950 absolute right-0 top-0 bottom-0" />
+      </div>
+    </main>
   );
+
 }
---
+++ src/components/movie-card.tsx
@@ -0,0 +1,44 @@
+import { getMovieUrl } from "@/utils/url";
+import {
+  TMDB_IMAGE_BASE_URL,
+  TMDB_IMAGE_SIZE_POSTER_MEDIUM,
+} from "@/lib/tmdb/constants";
+import Link from "next/link";
+import Image from "next/image";
+import { ImageIcon } from "lucide-react";
+
+type MovieCardProps = {
+  tmdb_id: number;
+  title: string;
+  poster_path: string | null;
+};
+
+export default function MovieCard(props: MovieCardProps) {
+  const { tmdb_id, title, poster_path } = props;
+
+  const href = getMovieUrl(tmdb_id, title);
+  const alt = `Poster of ${title}`;
+  const src = `${TMDB_IMAGE_BASE_URL}/${TMDB_IMAGE_SIZE_POSTER_MEDIUM}/${poster_path}`;
+
+  return (
+    <div className="relative h-full aspect-[2/3]">
+      <Link href={href}>
+        {poster_path === null ? (
+          <InvalidPoster />
+        ) : (
+          <Image alt={alt} src={src} fill className="object-cover" />
+        )}
+      </Link>
+    </div>
+  );
+}
+
+type InvalidPosterProps = {};
+
+function InvalidPoster(props: InvalidPosterProps) {
+  return (
+    <div className="w-full h-full grid place-items-center">
+      <ImageIcon className="w-8 h-8 opacity-50" />
+    </div>
+  );
+}
